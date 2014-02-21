a681a325eb71bb5c9f9238c46ddd0476b324147d
Maxence Labusquière <maxence.labusquiere@zenika.com>
2014-02-21 18:42:15 +0100

    add a resource folder fix pom

--- src/main/java/com/mkyong/common/controller/HelloController.java
+++ src/main/java/com/mkyong/common/controller/HelloController.java
@@ -4,19 +4,22 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
-@RequestMapping(value = "/welcome")
+@RequestMapping(value = "/aaa")
 public class HelloController {
 
-	@RequestMapping(value = "/welcome", method = RequestMethod.GET)
-	public String printWelcome(ModelMap model) {
+	@RequestMapping(method = RequestMethod.GET)
+	public ModelAndView printWelcome(ModelMap model) {
+        ModelAndView sb = new ModelAndView("hello");
+        System.out.println("bonjour");
+        model.addAttribute("message", "Spring 3 MVC Hello World");
+		return sb;
 
-		model.addAttribute("message", "Spring 3 MVC Hello World");
-		return "hello";
-
-	} 
-	// /welcome/welcome/welcome2
+	}
+/*	// /welcome/welcome/welcome2
 	@RequestMapping(value = "/welcome/welcome2", method = RequestMethod.GET)
 	public String printWelcome2(ModelMap model) {
 
@@ -31,5 +34,5 @@ public class HelloController {
 		model.addAttribute("message", "Spring 3 MVC Hello World 3");
 		return "hello";
 
-	}	
+	}	*/
 }
